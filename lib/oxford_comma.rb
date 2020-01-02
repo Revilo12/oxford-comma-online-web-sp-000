d181f284e4742177faffda13de6430f32e9badf8
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-02 23:06:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
 def oxford_comma(array)
-  length = array.length
-  array_back = array[-1]
-  array.pop 
-  array = array.join(", ")
-  array << "," unless length <= 2 
-  array << " and " + array_back unless length <= 1
-  array
+
 end
