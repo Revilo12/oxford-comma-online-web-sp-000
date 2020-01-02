628ea53cbd7e59abd6766b1f19879345da8eaca7
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-02 23:02:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   length = array.length
   array_back = array[-1]
   array.pop 
-  array.join(", ")
+  array.join!(", ")
   array << "," unless length == 2 
   array << "and " + array_back
   array
